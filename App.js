e9838d09f78440ecd38c0f8410c7addd40420020
Micalgenus <micalgenus@gmail.com>
2018-10-26 19:12:37 +0900

    console disable

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Muba from "./src/Routers";
 
-if (!__DEV__) console.disableYellowBox = true;
+console.disableYellowBox = true;
 
 export default class App extends Component {
   render() {
