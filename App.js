849111c84efc097b3885a98babe26e13ac6a211a
Micalgenus <micalgenus@gmail.com>
2018-11-19 21:15:38 +0900

    disable console warning

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Muba from "./src/Routers";
 
+console.disableYellowBox = true;
+
 export default class App extends Component {
   render() {
     return <Muba />;
